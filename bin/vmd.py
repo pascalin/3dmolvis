fe450bcd28660a4888dd1138ef8ffd4d0b818be3
David Suárez <david.suarez@yahoo.com>
2007-09-18 00:13:59 +0000

    Algunas peque~as modificaciones en el script vmd.py para que corra tambien en Irix.

--- bin/vmd.py
+++ bin/vmd.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
+"""A simple script to make a custom copy of VMD C-Shell startup
+script. It comments some lines and essentially disables the launching
+of an xterm previously to VMD booting. The custom copy is created on
+cwd or on command line indicated destination."""
 
-import sys, os
+import sys, os, re
 
 def _(s):
     return s
 
 if __name__ == "__main__":
-    if not sys.platform in ('linux2',):
-        sys.exit(0)
-    vmdpath = os.popen("whereis vmd").read().split()
-    if len(vmdpath)>1:
+    #Detect if we are in a *nix
+    if not re.search(r'^(linux|irix).*$', sys.platform):
+        sys.stderr.write(_("This script just functions on *nix OSes"))
+        sys.exit(1)
+    vmdpath = os.popen("whereis vmd").read().split() #Uses the whereis command
+    if len(vmdpath)>1: #VMD was found
         vmdpath=vmdpath[1]
     else:
         sys.stderr.write(_("You need to install VMD before you can employ VMDGui"))
@@ -27,17 +33,18 @@ if __name__ == "__main__":
     target.write("#!/bin/csh\n")
     comment = False
     lastline = ""
+    #Comment some lines
     for sourceline in [l for l in open(vmdpath) if not l.startswith("#")]:
         if comment:
-            if sourceline.strip() == "\"$MASTERVMDDIR/$execname\" $*" and "else" in lastline:
+            if sourceline.strip() == "\"$MASTERVMDDIR/$execname\" $*" and lastline.find("else")!=-1:
                 target.write(sourceline+"#\tendif\nendif\n")
                 break
             else:
                 target.write("#"+sourceline)
         else:
             target.write(sourceline)
-        if "bpsh $VMDSLAVENODE" in lastline:
+        if lastline.find("bpsh $VMDSLAVENODE")!=-1:
             comment = True
         lastline = sourceline
     target.close()
-    os.chmod(targetfile, 0755)
+    os.chmod(targetfile, 0755) #Makes the copy executable
